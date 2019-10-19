aeb74898053c4a1074c250dab38b51e0236fa55e
miguel-osuna <osuna.angulo.miguel@gmail.com>
2019-10-19 13:45:32 -0700

    Updates

---
+++ precache-manifest.8f25381e817ab8e479ecd1d2b61b5d21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884f5df636babb8cf684189ca2ec23bd",
+    "url": "/Robofriends-App/index.html"
+  },
+  {
+    "revision": "0b00f7920c85b916fc19",
+    "url": "/Robofriends-App/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f8095c1f9f276c863cb6",
+    "url": "/Robofriends-App/static/css/main.4e83edf0.chunk.css"
+  },
+  {
+    "revision": "0b00f7920c85b916fc19",
+    "url": "/Robofriends-App/static/js/2.f8c4f52c.chunk.js"
+  },
+  {
+    "revision": "f8095c1f9f276c863cb6",
+    "url": "/Robofriends-App/static/js/main.87f88a33.chunk.js"
+  },
+  {
+    "revision": "03937c9991fe50b1c5e3",
+    "url": "/Robofriends-App/static/js/runtime-main.561bea7d.js"
+  },
+  {
+    "revision": "d071190e622e1b361e8ca05d75da42e5",
+    "url": "/Robofriends-App/static/media/BelshawDonutRobot.d071190e.woff"
+  }
+]);
